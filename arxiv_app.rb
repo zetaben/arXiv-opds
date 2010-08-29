10b22c98496f764d11c3f98271ada3e90eb2c3bf
Benoit Larroque <zeta.ben@gmail.com>
2010-08-29 23:15:19 +0200

    Add HTML landing page

--- arxiv_app.rb
+++ arxiv_app.rb
@@ -4,12 +4,16 @@ require 'arxiv'
 require 'builder'
 require 'open-uri'
 require 'nokogiri'
+require 'haml'
+
+global_etag="v0"
 
 get '/' do
-	  "Hello from Sinatra on Heroku!"
+	haml :index
 end
 
 get '/catalog.atom' do
+	etag global_etag+'catalog'
 	content_type 'application/atom+xml', :charset => 'utf-8'
 	arxiv=ArXiv.new
 	builder :nav_feed, :locals => { :cats => arxiv.top.to_a,:arxiv => arxiv, :current_cat => 'root'}
@@ -17,6 +21,7 @@ end
 
 get '/subcats/*.atom' do
 	id=params[:splat].first
+	etag global_etag+"subcats_#{id}"
 	content_type 'application/atom+xml', :charset => 'utf-8'
 	arxiv=ArXiv.new
 	builder :nav_feed, :locals => { :cats => arxiv.subcats(id).to_a, :arxiv => arxiv, :current_cat => id}
@@ -25,6 +30,7 @@ end
 
 get '/feed/*.atom' do 
 	id=params[:splat].first
+	etag global_etag+"feed_#{id}_#{Time.now.to_i/3600}"
 	content_type 'application/atom+xml', :charset => 'utf-8'
 	arxiv=ArXiv.new
 	rdf_feed=Nokogiri::XML(open(arxiv.url(id)))
@@ -32,38 +38,39 @@ get '/feed/*.atom' do
 end
 
 get '/opensearchdescription.xml' do 
+	etag global_etag+"opensearch"
 	content_type 'application/xml'
 	builder :opensearch_description
 end
 
 get '/search/' do
 	redirect('/catalog.atom') if params[:q].nil?
+	etag global_etag+"search_#{params[q]}_#{params[:start]}_#{params[:max_results]}_#{Time.now.to_i/3600}"
 	url="http://export.arxiv.org/api/query?search_query=all:#{ params[:q]}"
 	burl=url.dup
 	url+="&start=#{params[:start]}" if params[:start]
 	url+="&max_results=#{params[:max_results]}" if params[:max_results]
 	feed=Nokogiri::XML(open(url))
-	
+
 	namespaces=feed.namespaces.to_hash.merge({ 'xmlns:opensearch'=>"http://a9.com/-/spec/opensearch/1.1/","xmlns"=>"http://www.w3.org/2005/Atom"})
-	STDERR.puts namespaces.inspect
 	total=feed.at('/xmlns:feed/opensearch:totalResults',namespaces).text.to_i
 	start=feed.at('/xmlns:feed/opensearch:startIndex',namespaces).text.to_i
 	perpage=feed.at('/xmlns:feed/opensearch:itemsPerPage',namespaces).text.to_i
 
 	feedel=feed.at('/xmlns:feed/xmlns:entry',namespaces)
 	unless start+perpage > total
-	link=Nokogiri::XML::Node.new('link',feed)
-	link['rel']='next'
-	link['title']='Next Page'
-	link['href']="/search/?q=#{ params[:q]}&start=#{(start+perpage)}"
-	feedel.add_previous_sibling(link)
+		link=Nokogiri::XML::Node.new('link',feed)
+		link['rel']='next'
+		link['title']='Next Page'
+		link['href']="/search/?q=#{ params[:q]}&start=#{(start+perpage)}"
+		feedel.add_previous_sibling(link)
 	end
 	unless start-perpage < 0
-	link=Nokogiri::XML::Node.new('link',feed)
-	link['rel']='prev'
-	link['title']='Previous Page'
-	link['href']="/search/?q=#{ params[:q]}&start=#{(start-perpage)}"
-	feedel.add_previous_sibling(link)
+		link=Nokogiri::XML::Node.new('link',feed)
+		link['rel']='prev'
+		link['title']='Previous Page'
+		link['href']="/search/?q=#{ params[:q]}&start=#{(start-perpage)}"
+		feedel.add_previous_sibling(link)
 	end
 
 	feed.xpath('/xmlns:feed/xmlns:entry',namespaces).each do |ent|
